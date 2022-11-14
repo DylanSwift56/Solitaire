13bb89215c2bb58497adb83652c0ed7233f59c4d
Dylan Swift <dylanswift56@gmail.com>
2022-11-14 16:54:56 +0000

    Commit

--- src/solitaire/GameScreen.java
+++ src/solitaire/GameScreen.java
@@ -5,22 +5,42 @@ import java.awt.*;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
-public class GameScreen extends JPanel{
+public class GameScreen extends JFrame{
     public GameScreen(){
-        super("Solitaire");
-        setSize(800, 600);
 
-        FlowLayout layout = new FlowLayout();
+        super("Solitaire");
+
+        JPanel mainPanel = new JPanel(new BorderLayout(2,2));
+        setBackground(Color.DARK_GRAY);
+
+       // mainPanel.setSize(100, 100);
+        setSize(800, 600);
+        JButton b1 = new JButton();
+        b1.setPreferredSize(new Dimension(50,100));
+        b1.setText("Card Stack");
+
+        JButton b2 = new JButton();
+        b2.setPreferredSize(new Dimension(50,100));
+        b2.setText("Card Stack");
+
+        JPanel cardStack = new JPanel(new BorderLayout(2,2));
+        cardStack.setPreferredSize(new Dimension(800, 100));
+        cardStack.setBackground(Color.BLACK);
+
+       // mainPanel.paint(Graphics(d));
+
+        cardStack.add(b1, BorderLayout.WEST);
+        cardStack.add(b2, BorderLayout.EAST);
+
+        mainPanel.add(cardStack, BorderLayout.NORTH);
+
+        BorderLayout layout = new BorderLayout(2, 2);
         setLayout(layout);
 
-        JButton b1 = new JButton();
-        b1.setPreferredSize(new Dimension(110,110));
-        b1.setText("Button");
-        add(b1);
-        JButton b2 = new JButton();
-        b2.setPreferredSize(new Dimension(110,110));
-        b2.setText("Button");
-        add(b2);
+        mainPanel.setBackground(Color.green);
+
+        add(mainPanel);
+
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);//from https://stackoverflow.com/questions/2442599/how-to-set-jframe-to-appear-centered-regardless-of-monitor-resolution
         addWindowListener(new WindowEventHandler());
