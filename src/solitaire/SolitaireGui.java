5621f70dc43b060f042e3c178243e794f3b2a230
Swift8301 <Swift8301@users.noreply.github.com>
2022-11-15 19:16:33 +0000

    Commit 4

--- src/solitaire/SolitaireGui.java
+++ src/solitaire/SolitaireGui.java
@@ -2,47 +2,74 @@ package solitaire;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
+import java.awt.event.*;
 
 public class SolitaireGui extends JFrame{
-    private JPanel panel1;
-    private JPanel cardStack;
+    private JPanel mainPanel;
+    private JToggleButton cardStack;
+    private JPanel cardStackPanel;
     private JPanel tableau;
     private JPanel foundations;
     private JPanel topPanel;
     private JPanel bottomPanel;
-    private JLabel foundation1;
-    private JLabel foundation2;
-    private JLabel foundation3;
-    private JLabel foundation4;
+    private JToggleButton foundation1;
+    private JToggleButton foundation2;
+    private JToggleButton foundation3;
+    private JToggleButton foundation4;
+    private JToggleButton discardPile;
 
     public SolitaireGui(){
-        setContentPane(panel1);
+        setContentPane(mainPanel);
         setTitle("Solitaire");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(new Dimension(800, 600));
+        setSize(new Dimension(900, 600));
         setLocationRelativeTo(null);//centers gui on the screen
         addWindowListener(new SolitaireGui.WindowEventHandler());
         JLabel card = new JLabel();
         card.setIcon(new ImageIcon("backOfCardSmall.jpg"));
+        setResizable(false);
+
+        cardStackPanel.setBackground(Color.BLUE);
+        foundations.setBackground(Color.BLUE);
+        bottomPanel.setBackground(Color.BLUE);
         cardStack.add(card);
+
 
         foundation1.setIcon(new ImageIcon("backOfCardSmall.jpg"));
         foundation2.setIcon(new ImageIcon("backOfCardSmall.jpg"));
         foundation3.setIcon(new ImageIcon("backOfCardSmall.jpg"));
         foundation4.setIcon(new ImageIcon("backOfCardSmall.jpg"));
 
+        cardStack.addActionListener(this::cardStackClk);
+        //foundation2.addActionListener(this::a);
+
         //foundation1.add(card);
 
         setVisible(true);
 
 
+
     }
+
+    public void cardStackClk(ActionEvent e){
+        discardPile.removeAll();
+        discardPile.setIcon(new ImageIcon("aceOfHearts.png"));
+        validate();
+    }
+
+    public void a(ActionEvent e){
+        if(e.getSource().toString().equals("foundation1"))
+            JOptionPane.showMessageDialog(null, "", "!!!", 1);
+
+    }
+
+
 
     public static void main(String[] args) {
         SolitaireGui gui = new SolitaireGui();
     }
+
+
 
 
     private class WindowEventHandler implements WindowListener {
